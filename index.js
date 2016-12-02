f46aaea19b4bece64ef1ea97a0789cf13c6b612a
H.T. Koo <h1017760@connect.hku.hk>
2016-12-03 01:00:10 +0800

    added an option in ejsOptions (useRelativePath:[true/false]) which would allow to use ejsOptions.parentPath as the path in 'filename' option during ejs.compile

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 var ejs = require('ejs');
 var path = require('path');
 
-var createTemplateName = function (basePath, parentPath, filePath) {	
-	var extensionRegex = /(\.[a-z]+)+$/;
-	var absolutePath = path.join(basePath, parentPath);
-	var normalizedAbsolutePath = absolutePath.lastIndexOf('/') == absolutePath.length - 1 
-					? absolutePath
-					: absolutePath + '/';
-	var templateName = filePath.replace(normalizedAbsolutePath, '')
-				   .replace(extensionRegex, '');
-	
-	return templateName;
+var createTemplateName = function (basePath, parentPath, filePath) {
+    var extensionRegex = /(\.[a-z]+)+$/;
+    var absolutePath = path.join(basePath, parentPath);
+    var normalizedAbsolutePath = absolutePath.lastIndexOf('/') == absolutePath.length - 1
+        ? absolutePath
+        : absolutePath + '/';
+    var templateName = filePath.replace(normalizedAbsolutePath, '')
+        .replace(extensionRegex, '');
+
+    return templateName;
 };
 
-var createEjsPreprocessor = function(logger, basePath, ejsOptions) {
+var createEjsPreprocessor = function (logger, basePath, ejsOptions) {
     var log = logger.create('preprocessor.ejs');
 
-    return function(content, file, done) {
+    return function (content, file, done) {
         var processed = null;
 
-	var templateName = createTemplateName(basePath, ejsOptions.parentPath, file.originalPath);
+        var templateName = createTemplateName(basePath, ejsOptions.parentPath, file.originalPath);
 
         log.debug('Processing "%s".', templateName);
 
         try {
+            var opts = {client: true};
+            if (ejsOptions.useRelativePath) {
+                opts[filename] = ejsOptions.parentPath + '/dummy.ejs';
+            }
+
             processed = "\
               (function() {\
                 this.JST || (this.JST = {});\
                 this.JST['" + templateName +
-                  "'] = " + ejs.compile(content, {client: true}) +
-            "}).call(this);";
+                "'] = " + ejs.compile(content, opts) +
+                "}).call(this);";
         } catch (e) {
             log.error('%s\n  at %s', e.message, content);
         }
